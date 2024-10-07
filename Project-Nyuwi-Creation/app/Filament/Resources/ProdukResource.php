6313b687a817077b02a66b5f0902a6e5d7440c7a
Vilzz <160743963+Vilamuzz@users.noreply.github.com>
2024-10-07 10:18:51 +0700

    Pembuatan CRUD-Sementara

---
+++ Project-Nyuwi-Creation/app/Filament/Resources/ProdukResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Produk;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Ramsey\Uuid\Type\Decimal;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ProdukResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProdukResource\RelationManagers;
+
+class ProdukResource extends Resource
+{
+    protected static ?string $model = Produk::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        TextInput::make('nama_produk')->required(),
+                        TextInput::make('harga')
+                            ->numeric()
+                            ->inputMode('decimal')
+                            ->required(),
+                        TextInput::make('stok')
+                            ->numeric()
+                            ->required(),
+                    ])
+                    ->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama_produk')->sortable()->searchable(),
+                TextColumn::make('harga')->sortable()->searchable(),
+                TextColumn::make('stok')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProduks::route('/'),
+            'create' => Pages\CreateProduk::route('/create'),
+            'edit' => Pages\EditProduk::route('/{record}/edit'),
+        ];
+    }
+}
